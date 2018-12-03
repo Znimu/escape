13045f4adddac72f80304292fdf3d4e0ffc15ea4
Blaise <b.morot.sir@gmail.com>
2018-12-03 15:49:02 +0100

    Creation projet base

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Projet Résumé Mois 1</title>
+				<meta charset="UTF-8">
+        <link rel='stylesheet' href="css/style.css" />
+				<link href="http://fonts.googleapis.com/css?family=Lato:200,400" rel="stylesheet" type="text/css">
+				<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+					<div class="background_header">
+						<img src="img/img_header.jpg" width="100%" />
+					</div>
+					
+					<div class="title_site"><i class="fas fa-paper-plane"></i> ESCAPE</div>
+			
+					<div>
+						<ul class="liste_menu">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="intro.php">Services</a></li>
+								<li><a href="linux.php">Clients</a></li>
+								<li><a href="wordpress.php">Team</a></li>
+								<li><a href="html.php">Contact</a></li>
+						</ul>
+					</div>
+					
+					<center>
+						<div class="subtitle_header">Can you build the website of my dreams ?</div>
+						<div class="title_header">YUP, WE CAN DO THAT.</div>
+					
+						<button class="btn_header">Learn more</button>
+					</center>
+					
+					<div class="clear_both"></div>
+					
+					<div id="icone_menu" class="icone_menu"></div>
+					<div id="icone_menu_close" class="icone_menu_close"></div>
+        </header>
+        
+        <main>
+					<div class="area1_main">
+						<div class="img_big float_right-img_big"><i class="fas fa-laptop-code"></i></div>
+						
+						<h1>Web <strong>Developement</strong></h1>
+				
+						<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam pellerentur ab urbe praecipites, sectatoribus disciplinarum liberalium inpendio paucis sine respiratione ulla extrusis, tenerentur minimarum adseclae veri, quique id simularunt ad tempus.<br /><br />Et tria milia saltatricum ne interpellata quidem cum choris totidemque remanerent magistris.</p>
+					</div>
+					
+
+					<div class="area2_main">
+						<div class="img_big float_left-img_big">
+							<i class="fas fa-fingerprint"></i>
+						</div>
+						
+						<h1>Identity <strong>Branding</strong></h1>
+				
+						<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam pellerentur ab urbe praecipites, sectatoribus disciplinarum liberalium inpendio paucis sine respiratione ulla extrusis, tenerentur minimarum adseclae veri, quique id simularunt ad tempus.<br /><br />Et tria milia saltatricum ne interpellata quidem cum choris totidemque remanerent magistris.</p>
+					</div>
+					
+					<div class="area3_main">
+						<div class="area3_main_area">
+							<div class="float_left-img_little">
+								<i class="fas fa-male"></i>
+							</div>
+							<h4>Branding & Identity</h4>
+							<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam.</p>
+						</div>
+						
+						<div class="area3_main_area">
+							<div class="float_left-img_little">
+								<i class="fas fa-pencil-alt"></i>
+							</div>
+							<h4>Web & Graphic Design</h4>
+							<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam.</p>
+						</div>
+						
+						<div class="area3_main_area">
+							<div class="float_left-img_little">
+								<i class="fas fa-mobile-alt"></i>
+							</div>
+							<h4>Mobile App Development</h4>
+							<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam.</p>
+						</div>
+						
+						<div class="area3_main_area">
+							<div class="float_left-img_little">
+								<i class="fas fa-images"></i>
+							</div>
+							<h4>Animations</h4>
+							<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam.</p>
+						</div>
+						
+						<div class="area3_main_area">
+							<div class="float_left-img_little">
+								<i class="fas fa-star-half-alt"></i>
+							</div>
+							<h4>UI / UX</h4>
+							<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam.</p>
+						</div>
+						
+						<div class="area3_main_area">
+							<div class="float_left-img_little">
+								<i class="fas fa-camera-retro"></i>
+							</div>
+							<h4>Photography</h4>
+							<p>Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam.</p>
+						</div>
+						
+						<div class="clear_both"></div>
+					</div>
+					
+					<div class="area4_main">
+						<center>
+							<h1 class="h1_10_margin">Awesome <strong>Clients</strong></h1>
+							<h5>See what nice things our clients said about us</h5>
+						</center>
+						
+						<div class="div_mind">
+							<img src="img/img_people1.jpg" class="img_people float_left" />
+							
+							<p class="p_float_left">&ldquo;Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam.&rdquo;<br /><br />
+							<span class="div_mind_source">- Ut cum peregrini ob formidatam.</span></p>
+						</div>
+						
+						<div class="clear_both"></div>
+						
+						<div class="div_mind">
+							<img src="img/img_people2.jpg" class="img_people float_right" />
+							
+							<p class="p_float_right">&ldquo;Postremo ad id indignitatis est ventum, ut cum peregrini ob formidatam haut ita dudum alimentorum inopiam.&rdquo;<br /><br />
+							<span class="div_mind_source">- Ut cum peregrini ob formidatam.</span></p>
+						</div>
+					</div>
+					
+					<div class="area3_main">
+						<center>
+							<img src="img/img_sponsor1.jpg" class="img_sponsors" />
+							<img src="img/img_sponsor2.jpg" class="img_sponsors" />
+							<img src="img/img_sponsor3.jpg" class="img_sponsors" />
+							<img src="img/img_sponsor4.jpg" class="img_sponsors" />
+						</center>
+					</div>
+					
+					<div class="area5_main">
+						<center>
+							<h1 class="h1_10_margin">Amazing <strong>Team</strong></h1>
+							<h5 class="grey_color">These wonderfull people make work enjoyable.</h5>
+						</center>
+					</div>
+				</main>
